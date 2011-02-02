3c5f0f8aeac28c210bc5056b3337355c0a017989
Elliot Chance <elliot@chancemedia.com>
2011-02-02 18:41:56 +1100

    Initial commit.

---
+++ src/net/eagledb/test/Main.java
@@ -0,0 +1,12 @@
+package net.eagledb.test;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
