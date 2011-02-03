3f557f6a658652b308dd7b5fe0ad5853bd38a366
Elliot Chance <elliot@chancemedia.com>
2011-02-04 01:46:52 +1100

    Added javacc tool to quickly regenerate parser code.

---
+++ tools/javacc.php
@@ -0,0 +1,13 @@
+<?php
+
+// delete old files
+$base = '../src/net/sf/jsqlparser/parser';
+@unlink("$base/TokenMgrError.java");
+@unlink("$base/ParseException.java");
+@unlink("$base/Token.java");
+@unlink("$base/SimpleCharStream.java");
+
+// run parser
+system("javacc-5.0/bin/javacc -OUTPUT_DIRECTORY=`pwd`/$base `pwd`/$base/JSqlParserCC.jj");
+
+?>
