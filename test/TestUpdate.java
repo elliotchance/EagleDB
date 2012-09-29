f18121ab267af4c2aed4b3e02901e928144a0e7a
Elliot Chance <elliotchance@gmail.com>
2012-09-29 19:28:08 +1000

    Update UPDATE test.

--- test/TestUpdate.java
+++ test/TestUpdate.java
@@ -1,9 +1,9 @@
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TestUpdate {
 
@@ -14,13 +14,6 @@ public class TestUpdate {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-		// create the temporary table used for these tests
-		TestSuiteEmbeddedDatabase.executeUpdate("create temp table test_update (id int, number double)");
-
-		// insert some test records
-		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (1, 1.0)");
-		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (2, 2.5)");
-		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (3, 8.7)");
     }
 
     @AfterClass
@@ -28,11 +21,19 @@ public class TestUpdate {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+		// create the temporary table used for these tests
+		TestSuiteEmbeddedDatabase.executeUpdate("create table test_update (id int, number double)");
+
+		// insert some test records
+		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (1, 1.0)");
+		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (2, 2.5)");
+		TestSuiteEmbeddedDatabase.executeUpdate("insert into test_update (id, number) values (3, 8.7)");
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
+		TestSuiteEmbeddedDatabase.executeUpdate("drop table test_update");
     }
 
     @Test
